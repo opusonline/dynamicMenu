c31afd4b2aba6f1978ff291bb7529893be174619
Stefan Benicke <stefan@noname.(none)>
2012-01-02 15:53:46 +0100

    added opening delay

--- jquery.dynamicMenu.js
+++ jquery.dynamicMenu.js
@@ -6,8 +6,9 @@
 (function($) {
 	
 	var defaults = {
-		timeout: 1000,
 		speed: 200,
+		delay: 350,
+		timeout: 1000,
 		showSubMenu: function() {
 			//$(this).addClass('open').prev().addClass('hover');
 			$(this).stop(true, true).slideDown(defaults.speed).prev().addClass('hover');
@@ -16,7 +17,8 @@
 			//$(this).removeClass('open').prev().removeClass('hover');
 			$(this).slideUp(defaults.speed).prev().removeClass('hover');
 		}
-	};
+	},
+	w = window;
 	
 	$.fn.dynamicMenu = function(options) {
 		
@@ -25,45 +27,45 @@
 		return this.each(function() {
 			
 			var $menu = $(this),
-			timer_list = [],
+			show_timer_list = [],
+			hide_timer_list = [],
 			_initSubMenu = function(id) {
 				var $list = $(this),
 				$sub = $list.children('ul').first();
 				if ( ! $sub.length) return;
 				var $parents = $list.parentsUntil($menu, 'li');
-				return $list.mouseenter(_showSubMenu).mouseleave(_timeoutHiding).data({id: id, sub: $sub, parents: $parents});
+				return $list.mouseenter(_timeoutShow).mouseleave(_timeoutHide).data({id: id, sub: $sub, parents: $parents});
 			},
-			_showSubMenu = function() {
+			_timeoutShow = function() {
 				var $list = $(this),
-				id = $list.data('id'),
-				$sub = $list.data('sub'),
+				data = $list.data(),
 				_clearParentsTimer = function() {
-					var id = $(this).data('id');
-					clearTimeout(timer_list[id]);
+					var parent_id = $(this).data('id');
+					clearTimeout(hide_timer_list[parent_id]);
+				},
+				_showSubMenu = function() {
+					options.showSubMenu.call(data.sub);
+					clearTimeout(hide_timer_list[data.id]);
+					_hideAllOtherMenuesInThisLevel($list, data.id);
 				};
-				options.showSubMenu.call($sub);
-				clearTimeout(timer_list[id]);
-				$list.data('parents').each(_clearParentsTimer);
-				_hideAllOtherMenuesInThisLevel($list, id);
+				data.parents.each(_clearParentsTimer);
+				show_timer_list[data.id] = w.setTimeout(_showSubMenu, options.delay);
 				return false;
 			},
-			_timeoutHiding = function() {
-				var $list = $(this),
-				id = $list.data('id'),
-				$sub = $list.data('sub'),
+			_timeoutHide = function() {
+				var data = $(this).data(),
 				_hideSubMenu = function() {
-					options.hideSubMenu.call($sub);
+					options.hideSubMenu.call(data.sub);
 				};
-				timer_list[id] = window.setTimeout(_hideSubMenu, options.timeout);
+				clearTimeout(show_timer_list[data.id]);
+				hide_timer_list[data.id] = w.setTimeout(_hideSubMenu, options.timeout);
 			},
 			_hideAllOtherMenuesInThisLevel = function($active_list, active_list_id) {
 				var _hideAllExceptActive = function() {
-					var $list = $(this),
-					id = $list.data('id'),
-					$sub = $list.data('sub');
-					if (id == active_list_id) return;
-					options.hideSubMenu.call($sub);
-					clearTimeout(timer_list[id]);
+					var data = $(this).data();
+					if (data.id == active_list_id) return;
+					options.hideSubMenu.call(data.sub);
+					clearTimeout(hide_timer_list[data.id]);
 				};
 				$active_list.parent().find(list).each(_hideAllExceptActive);
 			},
